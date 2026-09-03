ed0938aae69f62b2e989eec0d4bba9a658e6a4ce
Kang Wang <wk1984@users.noreply.github.com>
2026-09-03 22:53:26 +0800

    clean docker folder

--- docker/build_user_container.sh
+++
@@ -1,4 +0,0 @@
-docker build --progress=plain -f docker/User-Env.Dockerfile -t ecoon/watershed_workflow:master . && \
-docker build --progress=plain -f docker/ATS-User-Env.Dockerfile -t ecoon/watershed_workflow-ats:master .
-
-
